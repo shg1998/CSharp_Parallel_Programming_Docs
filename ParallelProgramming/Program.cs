91d084ab4fd6918370f949dd46380460f506dd92
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2022-06-01 19:31:40 +0430

    added Task Cancellation Sample :)

---
+++ ParallelProgramming/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming
+{
+    class Program
+    {
+        public static void Write(object o)
+        {
+            var i = 1000;
+            while (i-->0) Console.WriteLine(o);
+        }
+
+        public static int GetTextLength(object o)
+        {
+            Console.WriteLine($"\nTask with id : {Task.CurrentId} processing object : {o}");
+            return o.ToString().Length;
+        }
+        static void Main(string[] args)
+        {
+            //var t = new Task(Write, "salam");
+            //t.Start();
+            //Task.Factory.StartNew(Write, 12);
+            const string text1 = "testing";
+            const string text2 = "aleik";
+            var task1 = new Task<int>(GetTextLength, text1);
+            task1.Start();
+            var task2 = Task.Factory.StartNew<int>(GetTextLength, text2);
+            Console.WriteLine($"Length of {text1} is : {task1.Result}");
+            Console.WriteLine($"Length of {text2} is : {task2.Result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TaskCancellation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellation
+{
+    // how can i actually cancel this infinity task ? :)
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //// canonical way : 
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+            //token.Register(() =>
+            //{
+            //    Console.WriteLine("cancellation has been requested :)");
+            //});
+            //var t = new Task(() =>
+            //{
+            //    var inc = 0;
+            //    while (true)
+            //    {
+            //        //if(token.IsCancellationRequested)
+            //        //    throw new OperationCanceledException();
+            //        // == with above
+            //        token.ThrowIfCancellationRequested();
+            //        Console.WriteLine($"{inc++}\n");
+            //    }
+            //},token);
+            //t.Start();
+            //Task.Factory.StartNew(() =>
+            //{
+            //    token.WaitHandle.WaitOne();
+            //    Console.WriteLine("wait handle released,cancellation was requested :)");
+            //});
+            //Console.ReadKey();
+            //cts.Cancel();
+
+
+
+            // another : 
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(planned.Token,preventative.Token,emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                var i = 0;
+                while (true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\n");
+                    Thread.Sleep(1000);
+                }
+            },paranoid.Token);
+
+            Console.ReadKey();
+            emergency.Cancel();
+            
+            Console.ReadKey();
+        }
+    }
+}
